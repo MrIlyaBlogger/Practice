da1a63bc3962f347d1c90b610dfce6aea3a335cd
kids01 <ilya5111082467@gmail.com>
2024-03-16 14:10:42 +0300

    commit

--- 3.py
+++ 3.py
@@ -2,17 +2,17 @@
 import csv
 
 
-def search(iid, data):
+def search(iid, data): #создаём функцию поиска даты по шифру
     for stroka in data:
         if stroka[1] == iid:
             return stroka
 
 
-with open('rocket.txt',encoding='utf-8') as f:
+with open('rocket.txt',encoding='utf-8') as f: #открываем файл с данными
     data = list(csv.reader(f, delimiter='@'))
 
     iid = input()
-    while iid != 'nlo':
+    while iid != 'nlo': #ищем строку с подходящим шифром
         stroka = search(iid, data)
         if stroka is None:
             print('такой сигнал еще не был получен')
